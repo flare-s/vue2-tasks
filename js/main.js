e05cab337a837fe63a9e08831922c1ded27b3643
flare-s <webflarez@gmail.com>
2022-12-03 17:54:07 +0000

    Add delete task functionality

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ new Vue({
     priority: "1",
     tasks: [
       {
+        id: 1,
         label: "Go for a run",
         priority: 1,
       },
       {
+        id: 2,
         label: "Get groceries",
         priority: 5,
       },
@@ -16,13 +18,20 @@ new Vue({
   },
   methods: {
     addTask: function (event) {
+      let id =
+        this.tasks.length === 0 ? 1 : this.tasks[this.tasks.length - 1].id + 1;
       event.preventDefault();
       this.tasks.push({
+        id,
         label: this.newTask,
         priority: Number(this.priority),
       });
       this.newTask = "";
       this.priority = "1";
+    },
+    deleteTask: function (id) {
+      let taskToDeleteIndex = this.tasks.findIndex((task) => (task.id = id));
+      this.tasks.splice(taskToDeleteIndex, 1);
     },
   },
 });

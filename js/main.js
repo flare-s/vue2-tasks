fbcad7d20c513cf9717d2b4df1692974e33ff07b
flare-s <webflarez@gmail.com>
2022-12-03 17:29:12 +0000

    Display all tasks

---
+++ js/main.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: "#app",
+  data: {
+    tasks: [
+      {
+        label: "Go for a run",
+        priority: 1,
+      },
+      {
+        label: "Get groceries",
+        priority: 5,
+      },
+    ],
+  },
+});
